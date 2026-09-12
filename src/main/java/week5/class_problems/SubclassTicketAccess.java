c305646e81dfa41e8d262f06563656a001891f82
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 16:45:33 +0530

    Add Week 5 practice problem 2 - subclass access

---
+++ src/main/java/week5/class_problems/SubclassTicketAccess.java
@@ -0,0 +1,36 @@
+package week5.class_problems;
+
+public class SubclassTicketAccess {
+
+    static String classifyAccess(String fieldModifier, String accessorContext) {
+        if ("public".equals(fieldModifier)) {
+            return "ALLOWED";
+        }
+        if ("private".equals(fieldModifier) || "default".equals(fieldModifier)) {
+            return "SAME_CLASS".equals(accessorContext) && "private".equals(fieldModifier)
+                    ? "ALLOWED" : "DENIED";
+        }
+        if ("protected".equals(fieldModifier)) {
+            return "SAME_CLASS".equals(accessorContext)
+                    || "SAME_PACKAGE".equals(accessorContext)
+                    || "SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE".equals(accessorContext)
+                    ? "ALLOWED" : "DENIED";
+        }
+        return "DENIED";
+    }
+
+    static class MovieTicket {
+        protected double ticketPrice;
+    }
+
+    static class PremiumMovieTicket extends MovieTicket {
+        void showPrice() {
+            System.out.println(ticketPrice);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(classifyAccess("protected", "SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"));
+        System.out.println(classifyAccess("protected", "SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE"));
+    }
+}
