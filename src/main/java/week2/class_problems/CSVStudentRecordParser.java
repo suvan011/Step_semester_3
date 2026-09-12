a74e7be2e4e1ade77f31e4ec0e0dc65abac5e0dc
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 16:15:45 +0530

    Add Week 2 student record parser

---
+++ src/main/java/week2/class_problems/CSVStudentRecordParser.java
@@ -0,0 +1,19 @@
+package week2.class_problems;
+
+public class CSVStudentRecordParser {
+    public static void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        System.out.println("Name: " + fields[0] + " | Roll No: " + fields[1] + " | Dept: " + fields[2]);
+    }
+
+    public static void main(String[] args) {
+        parseStudentRecord("Ananya Verma,RA2211003010123,CSE");
+        parseStudentRecord("Ananya Verma,CSE");
+    }
+}
