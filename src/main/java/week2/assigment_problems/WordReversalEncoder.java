1c2a4ae50eeb10b6eff87df38f01fe3e964717d7
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 16:16:13 +0530

    Add Week 2 word reversal encoder

---
+++ src/main/java/week2/assigment_problems/WordReversalEncoder.java
@@ -0,0 +1,23 @@
+package week2.assigment_problems;
+
+public class WordReversalEncoder {
+    public static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversedWord = new StringBuilder(words[i]);
+            result.append(reversedWord.reverse());
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseEachWord("hello club"));
+    }
+}
