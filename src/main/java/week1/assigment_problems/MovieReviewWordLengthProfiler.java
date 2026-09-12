7cebf565a13049c99fdf191c99bab66eafe89409
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 14:59:55 +0530

    Add movie review word length assignment

---
+++ src/main/java/week1/assigment_problems/MovieReviewWordLengthProfiler.java
@@ -0,0 +1,28 @@
+package week1.assigment_problems;
+
+public class MovieReviewWordLengthProfiler {
+    public static void classifyWordLengths(String review) {
+        String[] words = review.trim().split("\\s+");
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (String word : words) {
+            int length = word.length();
+            if (length >= 1 && length <= 4) {
+                shortWords++;
+            } else if (length <= 8) {
+                mediumWords++;
+            } else {
+                longWords++;
+            }
+        }
+
+        System.out.println("Short: " + shortWords + " | Medium: " + mediumWords
+                + " | Long: " + longWords);
+    }
+
+    public static void main(String[] args) {
+        classifyWordLengths("This movie was absolutely fantastic and thrilling");
+    }
+}
