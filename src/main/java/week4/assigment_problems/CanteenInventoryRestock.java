7e5afc9407d33e0625ec09ffc271871b26d1a268
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 16:26:10 +0530

    Add Week 4 canteen inventory assignment

---
+++ src/main/java/week4/assigment_problems/CanteenInventoryRestock.java
@@ -0,0 +1,32 @@
+package week4.assigment_problems;
+
+public class CanteenInventoryRestock {
+    static class Item {
+        private String itemName;
+        private int stock;
+
+        public Item(String itemName, int stock) {
+            this.itemName = itemName;
+            this.stock = stock;
+        }
+
+        public void restock(int stock) {
+            this.stock += stock;
+        }
+
+        public void printStock() {
+            System.out.println(itemName + " | Final Stock: " + stock);
+        }
+    }
+
+    public static void main(String[] args) {
+        Item[] items = {
+            new Item("Samosa", 15), new Item("Tea Powder", 40),
+            new Item("Bread", 8), new Item("Biscuit Packs", 25)
+        };
+        for (Item item : items) {
+            item.restock(20);
+            item.printStock();
+        }
+    }
+}
