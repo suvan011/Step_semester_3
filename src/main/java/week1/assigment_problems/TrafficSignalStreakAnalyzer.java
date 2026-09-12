aca52e9216670ad4b75c7d041b91501c8dbf50c1
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-12 14:59:44 +0530

    Add traffic signal streak assignment

---
+++ src/main/java/week1/assigment_problems/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,34 @@
+package week1.assigment_problems;
+
+public class TrafficSignalStreakAnalyzer {
+    public static void findLongestStreak(String signalLog) {
+        if (signalLog == null || signalLog.isEmpty()) {
+            System.out.println("Signal log is empty.");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int longestStreak = 1;
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentStreak++;
+            } else {
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor + "' repeated " + longestStreak + " times");
+    }
+
+    public static void main(String[] args) {
+        findLongestStreak("RRGGGYRR");
+        findLongestStreak("RRRRYYGG");
+    }
+}
