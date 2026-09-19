4b51ec254c857972cc74fba1d512e4d538d46bd0
SUVAN GUPTA <sg.suvangupta011@gmail.com>
2026-09-19 16:05:25 +0530

    Add Week 7 Category B practice problem

---
+++ src/main/java/week7/class_problems/SmartKitchenAssistant.java
@@ -0,0 +1,55 @@
+package week7.class_problems;
+
+public class SmartKitchenAssistant {
+    static abstract class KitchenTool {
+        private int speedLevel = 1;
+
+        public KitchenTool() {
+        }
+
+        public abstract String prepare();
+
+        public int getSpeedLevel() {
+            return speedLevel;
+        }
+
+        public void setSpeedLevel(int speedLevel) {
+            if (speedLevel >= 1 && speedLevel <= 5) {
+                this.speedLevel = speedLevel;
+            }
+        }
+    }
+
+    interface Washable {
+        String clean();
+    }
+
+    static class Blender extends KitchenTool implements Washable {
+        public Blender() {
+            super();
+        }
+
+        @Override
+        public String prepare() {
+            return "Blending at speed " + getSpeedLevel();
+        }
+
+        @Override
+        public String clean() {
+            return "Blender rinsed and dried";
+        }
+    }
+
+    public static void main(String[] args) {
+        Blender b = new Blender();
+
+        b.setSpeedLevel(3);
+        System.out.println(b.getSpeedLevel());
+
+        b.setSpeedLevel(9);
+        System.out.println(b.getSpeedLevel());
+
+        System.out.println(b.prepare());
+        System.out.println(b.clean());
+    }
+}
